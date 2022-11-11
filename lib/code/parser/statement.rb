36f5e1f9130f93b79f3b4af9b475b7323cdd7f38
Dorian Marié <dorian@dorianmarie.fr>
2022-11-11 17:58:00 +0100

    Range

--- lib/code/parser/statement.rb
+++ lib/code/parser/statement.rb
@@ -2,7 +2,7 @@ class Code
   class Parser
     class Statement < ::Code::Parser
       def parse
-        parse_subclass(::Code::Parser::OrOperator)
+        parse_subclass(::Code::Parser::Range)
       end
     end
   end
---
+++ spec/code/parser/range_spec.rb
@@ -0,0 +1,24 @@
+require "spec_helper"
+
+RSpec.describe ::Code::Parser do
+  subject { ::Code::Parser.parse(input) }
+
+  ["a..b"].each do |input|
+    context input do
+      let!(:input) { input }
+
+      it { expect { subject }.to_not raise_error }
+    end
+  end
+
+  [
+    "a /* cool */ .. b",
+    "a .. /* cool */ b",
+  ].each do |input|
+    context input do
+      let!(:input) { input }
+
+      it { expect(subject.to_json).to include("cool") }
+    end
+  end
+end
