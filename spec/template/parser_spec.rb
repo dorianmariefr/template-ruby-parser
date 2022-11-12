1b1a7cf6d75877c37bbdc4d5ffa8a188eab3999f
Dorian Marié <dorian@dorianmarie.fr>
2022-11-12 14:25:48 +0100

    Template parser

---
+++ spec/template/parser_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+RSpec.describe ::Template::Parser do
+  subject { ::Template::Parser.parse(input) }
+
+  [
+    "Hello",
+    "Hello {name}",
+    "Hello \\{name}",
+  ].each do |input|
+    context input do
+      let!(:input) { input }
+
+      it { expect { subject }.to_not raise_error }
+    end
+  end
+end
