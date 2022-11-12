21cba9fe376af056b6cdbf55233498036133d428
Dorian Marié <dorian@dorianmarie.fr>
2022-11-12 10:20:00 +0100

    While

--- lib/code/parser/statement.rb
+++ lib/code/parser/statement.rb
@@ -2,7 +2,7 @@ class Code
   class Parser
     class Statement < ::Code::Parser
       def parse
-        parse_subclass(::Code::Parser::IfModifier)
+        parse_subclass(::Code::Parser::While)
       end
     end
   end
